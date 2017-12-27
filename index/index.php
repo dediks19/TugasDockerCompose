0fa8a7ca5c43b45f675b6897c25821daa7238e96
tikaSadega <33304335+tikaSadega@users.noreply.github.com>
2017-12-27 22:57:10 +0700

    update

---
+++ index/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>dediks19</title>
+</head>
+<body>
+	<h2>Tugas Teori TCC Docker Compose</h2>
+	<h5><br>Nama    : Dedik Setyawan</br></h5>
+	<h5>Nim		: 155410006</h5>
+	<h5>Git Hub : https://github.com/dediks19</h5>
+	
+</body>
+</html>
